ed8f37c0c8102f7976ee9a6f51ec5913b50b980d
Jayashree <jk4418@nyu.edu>
2015-01-06 17:52:48 -0500

    Add functions to this module to generalize to all CNG repeats as well as to fin  max number of CNG repeats found throughout the genome

--- ManipulateSeqData/findrepeats.py
+++ ManipulateSeqData/findrepeats.py
@@ -24,22 +24,53 @@ def extractChromosomeSeq(fastafile,chromosome):
     for element in chrmconvert.keys():
         if chrmconvert[element] == chromosome:
             chrm = element
-            print chrm
     rec = [record for record in SeqIO.parse(open(fastafile,'rU'),'fasta') if record.name == chrm]
     return str(rec[0].seq)
     
 # This function finds indexes for where CAG repeats starts. The parameters 
 # for this function, seq, the number of CAG repeats to find
-def extractIndexCAGrepeats(seq,numrepeats):
-    regexexpr = "(?<!(CAG))(CAG){" + str(numrepeats) +"}(?!(CAG))"
-    regs = re.finditer(regexexpr,seq)
-    return [[i.start(0),i.end(0)] for i in regs]
+#def extractIndexCAGrepeats(seq,numrepeats):
+    #regexexpr = "(?<!(CAG))(CAG){" + str(numrepeats) +"}(?!(CAG))"
+    #regs = re.finditer(regexexpr,seq)
+    #return [[i.start(0),i.end(0)] for i in regs]
     
 
 # This function combines the above two functions were it finds the CAG repeats 
 # for a fastafile for a chromosome
-def getCAGRepeatsForChromsome(fastafile,chromosome,numrepeats):
+#def getCAGRepeatsForChromsome(fastafile,chromosome,numrepeats):
+    #seq = extractChromosomeSeq(fastafile,chromosome)
+    #return extractIndexCAGrepeats(seq,numrepeats)
+
+# This function finds indexes for any three letter repeats. The parametes 
+# for this function, seq, the three letter repeat, number of three letter repeats
+def extractIndexrepeats(seq,repeat,numrepeats):
+    regexexpr = "(?<!(" + repeat + "))(" + repeat + "){" + str(numrepeats) +"}(?!(" + repeat + "))"
+    regs = re.finditer(regexexpr,seq)
+    return [[i.start(0),i.end(0)] for i in regs]
+
+# This function combines the above two functions where it finds the trinucleotide repeats 
+# for a fastafile for a chromosome
+def getRepeatsForChromsome(fastafile,chromosome,repeat,numrepeats):
     seq = extractChromosomeSeq(fastafile,chromosome)
-    return extractIndexCAGrepeats(seq,numrepeats)
-    
-# This function find indexes for where homopolymer runs occur
+    return extractIndexrepeats(seq,repeat,numrepeats)
+
+# This function gives the total lengths of trinucleotide repeats found for 
+# each chromosome
+def extractLengthsTrinucleotideRepeats(fastafile,chromosome,repeat,retlength):
+    seq = extractChromosomeSeq(fastafile,chromosome)
+    regexexpr = "(" + repeat + "){3,}"    
+    regs = re.finditer(regexexpr,seq)
+    if retlength:
+        return [i.end(0)-i.start(0) for i in regs]
+    else:
+        return [[i.start(0),i.end(0)] for i in regs]
+
+# This function returns the max number of trinucleotide repeats found for all chromosomes
+def maxLengthTrinucleotideRepeat(fastafile,repeat):
+    maxlengtheachchrom = []
+    for c in range(1,17):
+        chromosome = str(c)
+        lengthsPerChrom = extractLengthsTrinucleotideRepeats(fastafile,chromosome,repeat,True)
+        if len(lengthsPerChrom)!=0:
+            maxlengtheachchrom.append(max(lengthsPerChrom))
+    return maxlengtheachchrom
